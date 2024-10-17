9e5eae41e693623661b70a12aa3aa7ef8b894e8a
zhonggs <zhonggs@infore.com>
2024-10-17 13:54:34 +0800

    修改游戏逻辑

--- src/Game.js
+++ src/Game.js
@@ -1,68 +1,74 @@
 // src/App.js
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
-import './Game.css'
+import "./Game.css";
 
-const socket = io('http://localhost:3456/game');
+const socket = io("http://localhost:3456/game");
 
 function Game() {
   const [team, setTeam] = useState(null);
   const [ropePosition, setRopePosition] = useState(0);
   const [gameStarted, setGameStarted] = useState(false);
   const [countdown, setCountdown] = useState(null);
-  const [message, setMessage] = useState('');
-  const [username, setUsername] = useState('');
+  const [message, setMessage] = useState("");
+  const [username, setUsername] = useState("");
   const [history, setHistory] = useState({});
   const [animatePress, setAnimatePress] = useState(false);
 
   useEffect(() => {
-    socket.on('gameStart', () => {
+    socket.on("gameStart", () => {
       setGameStarted(true);
       setCountdown(null);
-      setMessage('Game started! Press the button!');
+      setMessage("游戏已经开始，请按下press按钮!");
+    });
+    socket.on("gameJoined", () => {
+      setMessage("已经加入游戏，等待游戏开始");
     });
 
-    socket.on('updateRope', (data) => {
+    socket.on("updateRope", (data) => {
       setRopePosition(data.position);
     });
 
-    socket.on('gameEnd', (data) => {
+    socket.on("gameEnd", (data) => {
       setGameStarted(false);
-      setMessage(`${data.winningTeam} team wins!`);
+      setMessage(`${data.winningTeam} 队伍赢了!`);
     });
 
-    socket.on('countdown', (timeLeft) => {
+    socket.on("countdown", (timeLeft) => {
+      console.log(timeLeft)
+
       setCountdown(timeLeft);
-      setMessage(`Game starting in ${timeLeft} seconds`);
+      setMessage(`游戏 ${timeLeft} 秒后开始`);
     });
 
     return () => {
-      socket.off('gameStart');
-      socket.off('updateRope');
-      socket.off('gameEnd');
-      socket.off('countdown');
+      socket.off("gameStart");
+      socket.off("updateRope");
+      socket.off("gameEnd");
+      socket.off("countdown");
+      socket.off("gameJoined");
+
     };
   }, []);
 
   const joinGame = (selectedTeam) => {
-    socket.emit('joinGame', { username, team: selectedTeam });
+    socket.emit("joinGame", { username, team: selectedTeam });
     setTeam(selectedTeam);
   };
 
   const handlePress = () => {
-    socket.emit('press');
+    socket.emit("press");
     setAnimatePress(true);
     setTimeout(() => setAnimatePress(false), 100); // 短暂的拉伸动画
   };
 
   const fetchHistory = async () => {
     if (username) {
-      const url = `http://localhost:3456/game/history/${username}`
+      const url = `http://localhost:3456/game/history/${username}`;
       const response = await fetch(url);
       const data = await response.json();
       setHistory(data);
     }
-    
   };
 
   return (
@@ -73,57 +79,66 @@ function Game() {
         placeholder="用户名"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
-        style={{display: "block", margin: "0 auto"}}
+        style={{ display: "block", margin: "0 auto" }}
       />
-     
-     
+
       {!team ? (
         <div>
-          <button onClick={() => joinGame('left')}>加入左边队伍</button>
-          <button onClick={() => joinGame('right')}>加入右边队伍</button>
+          <button onClick={() => joinGame("left")}>加入左边队伍</button>
+          <button onClick={() => joinGame("right")}>加入右边队伍</button>
         </div>
       ) : (
         <div>
           <p>你的队伍: {team}</p>
-          {countdown !== null && !gameStarted ? (
+          <p>{message}</p>
+          {/* {countdown !== null && !gameStarted ? (
             <p>{message}</p>
           ) : (
-            <>
-              {gameStarted ? (
-                <button
-                  onClick={handlePress}
-                  style={{ animation: animatePress ? 'pullAnimation 0.2s ease' : '' }}
-                >
-                  Press!
-                </button>
-              ) : (
-                <p>{message}</p>
-              )}
-        
-                <div className="rope-container" style={{ transform: `translateX(${ropePosition * 10}px)` }}>
-                  <div className="line"></div>
-                  <div className="character left">
-                    <span role="img" aria-label="Left Team">👦</span>
-                  </div>
-                  <div
-                    className="rope"
-                  >
-                    H
-                  </div>
-                  <div className="character right">
-                    <span role="img" aria-label="Right Team">👧</span>
-                  </div>
-                </div>
-              
-              <div>
-                <p>Rope Position: {ropePosition}</p>
-              </div>
-            </>
-          )}
+           null
+          )} */}
         </div>
       )}
+      <>
+        {gameStarted ? (
+          <button
+            onClick={handlePress}
+            style={{ animation: animatePress ? "pullAnimation 0.2s ease" : "" }}
+          >
+            Press!
+          </button>
+        ) : null}
+
+        <div
+          className="rope-container"
+          style={{ transform: `translateX(${ropePosition * 10}px)` }}
+        >
+          <div className="line"></div>
+          <div className="character left">
+            <span role="img" aria-label="Left Team">
+              👦
+            </span>
+          </div>
+          <div className="rope">H</div>
+          <div className="character right">
+            <span role="img" aria-label="Right Team">
+              👧
+            </span>
+          </div>
+        </div>
+
+        <div>
+          <p>分数: {ropePosition}</p>
+        </div>
+      </>
       <button onClick={() => fetchHistory()}>查看比赛历史记录</button>
-      <ul style={{width: "500px", padding: 0, margin: "0 auto", listStyle: "none"}}>
+      <ul
+        style={{
+          width: "500px",
+          padding: 0,
+          margin: "0 auto",
+          listStyle: "none",
+        }}
+      >
         {history?.history?.map((record, index) => (
           <li key={index}>
             {history.username} - {record.result} - {record.presses} presses
